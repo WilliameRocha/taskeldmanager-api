9cd097a28ea1903a91c74849ff6c7fa4b19ffcce
Williame Rocha <williame.rocha10@gmail.com>
2025-03-16 21:02:04 -0300

    Unit Test to create User and perform login. Controller, service and repository are covered

--- src/test/java/com/eldorado/task/manager/taskeldmanager/TaskEldManagerApplicationTests.java
+++ src/test/java/com/eldorado/task/manager/taskeldmanager/TaskEldManagerApplicationTests.java
@@ -1,13 +1,24 @@
-package com.eldorado.task.manager.taskeldmanager;
+package com.eldorado.task.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ComponentScan(basePackages = {"com.eldorado"})
 class TaskEldManagerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Configuration
+    @ComponentScan(basePackages = "com")  // Define o escaneamento de pacotes
+    static class TestConfig {
+        // Configurações de teste adicionais, se necessário
     }
 
 }
